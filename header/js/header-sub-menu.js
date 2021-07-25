51960111e5447d9fab4a60d91782532e3551d5d8
робин робин <myswamp0505@gmail.com>
2021-07-25 21:51:12 +0300

    header wip

---
+++ header/js/header-sub-menu.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".artstyle-list__link").forEach(item => {
+  item.addEventListener("click", function() {
+    let listOpen = this.parentElement.querySelector(".artstyle-sub-list");
+    this.classList.toggle("active-art-list-item");
+    listOpen.classList.toggle("active-sub-list");
+    document.querySelectorAll(".artstyle-sub-list").forEach(element => {
+      if (listOpen !=element) {
+        element.classList.remove("active-art-list-item");
+      }
+    });
+  });
+});
