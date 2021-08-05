45a5566c582ef65ff4a74cf9b345b8c5c23117fc
робин робин <myswamp0505@gmail.com>
2021-08-05 15:35:37 +0300

    header and hero, starting gallery

--- header/js/burger-search-form.js
+++ header/js/burger-search-form.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
-  $('.search-box__icon_upper').click(function(event) {
-    $('.header__form-upper,.search-box__input_upper').toggleClass('active');
-    $('.header-burger,.header__logo').toggleClass('closed');
+  $('.header-upper__form-enable').click(function(event) {
+    $('.header-upper__form').toggleClass('active');
+    $('.header-upper__form-enable').toggleClass('disabled');
   });
 });
+
+  const mediaQuery = window.matchMedia('(min-width: 576px) and (max-width: 992px)');
+  if (mediaQuery.matches) {
+    $(document).ready(function() {
+      $('.header-upper__form-enable').click(function(event) {
+        $('.header-burger,.header__logo').toggleClass('hidden');
+      });
+    });
+  }
+
